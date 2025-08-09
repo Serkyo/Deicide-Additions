bc2dfc71786774fadbda277200bc124860012ec7
Serkyo <serkyo-dev@proton.me>
2025-08-09 19:06:00 +0200

    Changed the behaviour of multiple damage types to scale on max health

---
+++ src/main/java/com/serkyo/deicideadditions/event/ServerEvents.java
@@ -0,0 +1,25 @@
+package com.serkyo.deicideadditions.event;
+
+import com.serkyo.deicideadditions.DeicideAdditions;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = DeicideAdditions.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ServerEvents {
+    @SubscribeEvent
+    public static void onLivingDamage(LivingDamageEvent event) {
+        DamageSource source = event.getSource();
+        LivingEntity entity = event.getEntity();
+
+        if (entity instanceof Player){
+            if (source.is(DamageTypes.STARVE) || source.is(DamageTypes.IN_WALL) || source.is(DamageTypes.DROWN) || source.is(DamageTypes.WITHER) || source.is(DamageTypes.ON_FIRE) || source.is(DamageTypes.FREEZE)) {;
+                event.setAmount((float) Math.max(event.getAmount(), Math.ceil(event.getAmount() / 20 * entity.getMaxHealth() / 4)));
+            }
+        }
+    }
+}
