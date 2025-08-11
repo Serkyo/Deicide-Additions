d78466000a47a4725c8614aca00501e577cebd82
Serkyo <prousttom@gmail.com>
2025-08-11 21:27:53 +0200

    Modified instant damage and instant health to scale on max hp %

--- src/main/java/com/serkyo/deicideadditions/mixin/FoodDataMixin.java
+++ src/main/java/com/serkyo/deicideadditions/mixin/FoodDataMixin.java
@@ -11,12 +11,12 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public class FoodDataMixin {
 
     @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;heal(F)V", ordinal = 0))
-    public float modifyPassiveHealing1(float defaultValue, @Local(argsOnly = true) Player player){
+    public float modifyPassiveHealing1(float defaultValue, @Local(argsOnly = true) Player player) {
         return (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth() / 3));
     }
 
     @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;heal(F)V", ordinal = 1))
-    public float modifyPassiveHealing2(float defaultValue, @Local(argsOnly = true) Player player){
+    public float modifyPassiveHealing2(float defaultValue, @Local(argsOnly = true) Player player) {
         return (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth() / 3));
     }
 }
