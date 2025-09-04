d46decf0ae3df3ab12b72593c3dc2d1b91933de3
Serkyo <prousttom@gmail.com>
2025-09-05 01:47:21 +0200

    Merged the functionality of campfire applying comfort with the mod

--- src/main/java/com/serkyo/deicideadditions/event/ServerEvents.java
+++ src/main/java/com/serkyo/deicideadditions/event/ServerEvents.java
@@ -7,6 +7,7 @@ import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
@@ -41,10 +42,11 @@ public class ServerEvents {
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
         if (event.phase == TickEvent.Phase.END) {
             Player player = event.player;
-            if (player.tickCount % 40 == 0) {
-                boolean nearBoss = !player.level().getEntitiesOfClass(LivingEntity.class, player.getBoundingBox().inflate(64), e -> e.getType().is(Tags.EntityTypes.BOSSES)).isEmpty();
+            if (player.tickCount % 80 == 0) {
+                AABB bounding_box = player.getBoundingBox().inflate(64);
+                boolean nearBoss = !player.level().getEntitiesOfClass(LivingEntity.class, bounding_box, e -> e.getType().is(Tags.EntityTypes.BOSSES)).isEmpty();
                 if (nearBoss) {
-                    player.addEffect(new MobEffectInstance(DeicideEffects.CORRUPTING_PRESENCE_EFFECT.get(), 100, 0, true, true));
+                    player.addEffect(new MobEffectInstance(DeicideEffects.CORRUPTING_PRESENCE_EFFECT.get(), 200, 0, true, true));
                 }
             }
         }
---
+++ src/main/java/com/serkyo/deicideadditions/mixin/CampfireBlockEntityMixin.java
@@ -0,0 +1,35 @@
+package com.serkyo.deicideadditions.mixin;
+
+import com.serkyo.deicideadditions.core.DeicideEffects;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.CampfireBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import vectorwing.farmersdelight.common.registry.ModEffects;
+
+import java.util.List;
+
+@Mixin(CampfireBlockEntity.class)
+public class CampfireBlockEntityMixin {
+
+    @Inject(method = "cookTick", at = @At(value = "HEAD"))
+    private static void campfireEffect(Level level, BlockPos pos, BlockState state, CampfireBlockEntity blockEntity, CallbackInfo ci) {
+        if (level.getGameTime() % 40 == 0) {
+            AABB bounding_box = new AABB(pos).inflate(3);
+            List<Player> nearby_players = level.getEntitiesOfClass(Player.class, bounding_box);
+
+            for (Player player : nearby_players) {
+                if (!player.hasEffect(DeicideEffects.CORRUPTING_PRESENCE_EFFECT.get())) {
+                    player.addEffect(new MobEffectInstance(ModEffects.COMFORT.get(), 200, 0, true, true));
+                }
+            }
+        }
+    }
+}
