38f76359407bd49c566c9677eab24105d38466a7
Serkyo <serkyo-dev@proton.me>
2025-09-05 04:37:43 +0200

    Modified the healing spells of ISS to heal body parts

---
+++ src/main/java/com/serkyo/deicideadditions/mixin/spells/BlessingOfLifeMixin.java
@@ -0,0 +1,29 @@
+package com.serkyo.deicideadditions.mixin.spells;
+
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.spells.CastSource;
+import io.redspace.ironsspellbooks.capabilities.magic.TargetEntityCastData;
+import io.redspace.ironsspellbooks.spells.holy.BlessingOfLifeSpell;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import sfiomn.legendarysurvivaloverhaul.registry.MobEffectRegistry;
+
+@Mixin(BlessingOfLifeSpell.class)
+public class BlessingOfLifeMixin {
+    @Inject(method = "onCast", at = @At(value = "TAIL"), remap = false)
+    private void painkillerEffect(Level world, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData, CallbackInfo ci) {
+        if (playerMagicData.getAdditionalCastData() instanceof TargetEntityCastData healTargetingData) {
+            var targetEntity = healTargetingData.getTarget((ServerLevel) world);
+            if (targetEntity instanceof Player) {
+                targetEntity.addEffect(new MobEffectInstance(MobEffectRegistry.PAINKILLER.get(), spellLevel * 20, spellLevel / 3));
+            }
+        }
+    }
+}
---
+++ src/main/java/com/serkyo/deicideadditions/mixin/spells/HealSpellMixin.java
@@ -0,0 +1,41 @@
+package com.serkyo.deicideadditions.mixin.spells;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.spells.CastSource;
+import io.redspace.ironsspellbooks.spells.holy.HealSpell;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import sfiomn.legendarysurvivaloverhaul.api.bodydamage.BodyDamageUtil;
+import sfiomn.legendarysurvivaloverhaul.api.bodydamage.BodyPartEnum;
+import sfiomn.legendarysurvivaloverhaul.config.Config;
+
+import java.util.Map;
+
+@Mixin(HealSpell.class)
+public class HealSpellMixin {
+    @Inject(method = "onCast", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;heal(F)V"), remap = false)
+    private void bodyPartScaling(Level world, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData, CallbackInfo ci, @Local float healAmount) {
+        if (entity instanceof Player player) {
+            Map<BodyPartEnum, Float> healthPercentages = Map.of(
+                    BodyPartEnum.HEAD, (float) Config.Baked.headPartHealth,
+                    BodyPartEnum.CHEST, (float) Config.Baked.chestPartHealth,
+                    BodyPartEnum.LEFT_ARM, (float) Config.Baked.armsPartHealth,
+                    BodyPartEnum.RIGHT_ARM, (float) Config.Baked.armsPartHealth,
+                    BodyPartEnum.LEFT_LEG, (float) Config.Baked.legsPartHealth,
+                    BodyPartEnum.RIGHT_LEG, (float) Config.Baked.legsPartHealth,
+                    BodyPartEnum.LEFT_FOOT, (float) Config.Baked.feetPartHealth,
+                    BodyPartEnum.RIGHT_FOOT, (float) Config.Baked.feetPartHealth
+            );
+            for(BodyPartEnum part : BodyPartEnum.values()) {
+                BodyDamageUtil.healBodyPart(player, part, healAmount / 2 * healthPercentages.get(part));
+                System.out.println("Healed " + part.name() + " for " + healAmount / 2 * healthPercentages.get(part));
+            }
+        }
+    }
+}
