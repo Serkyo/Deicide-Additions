38794ee62b2f03d560f4bc50b986969277831502
Serkyo <serkyo-dev@proton.me>
2025-09-22 20:47:44 +0200

    Added title textures, as well as a new recipe

---
+++ src/main/java/com/serkyo/deicideadditions/core/DeicideSavedData.java
@@ -0,0 +1,18 @@
+//package com.serkyo.deicideadditions.core;
+//
+//import net.minecraft.nbt.CompoundTag;
+//import net.minecraft.world.entity.LivingEntity;
+//import net.minecraft.world.level.saveddata.SavedData;
+//
+//import java.util.ArrayList;
+//
+//public class DeicideSavedData extends SavedData {
+//    private static
+//
+//    @Override
+//    public CompoundTag save(CompoundTag tag) {
+//        return null;
+//    }
+//
+//    public static DeicideSavedData load(CompoundTag tag)
+//}
--- src/main/java/com/serkyo/deicideadditions/datagen/DeicideRecipeProvider.java
+++ src/main/java/com/serkyo/deicideadditions/datagen/DeicideRecipeProvider.java
@@ -1,5 +1,6 @@
 package com.serkyo.deicideadditions.datagen;
 
+import com.github.sculkhorde.core.ModItems;
 import com.serkyo.deicideadditions.core.DeicideItems;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.data.PackOutput;
@@ -17,10 +18,17 @@ public class DeicideRecipeProvider extends RecipeProvider implements IConditionB
 
     @Override
     protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Items.SCULK, 8)
+                .define('#', Items.MOSS_BLOCK)
+                .define('X', ModItems.SCULK_RESIN.get())
+                .pattern("###")
+                .pattern("#X#")
+                .pattern("###")
+                .unlockedBy("has_sculk_resin", inventoryTrigger(ItemPredicate.Builder.item().of(ModItems.SCULK_RESIN.get()).build()))
+                .save(pWriter);
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, DeicideItems.LEATHER_SCRAP.get(), 3)
                 .requires(Items.LEATHER)
-                .unlockedBy("has_leather", inventoryTrigger(ItemPredicate.Builder.item().
-                        of(Items.LEATHER).build()))
+                .unlockedBy("has_leather", inventoryTrigger(ItemPredicate.Builder.item().of(Items.LEATHER).build()))
                 .save(pWriter);
         SimpleCookingRecipeBuilder.smelting(Ingredient.of(Items.ROTTEN_FLESH), RecipeCategory.MISC, DeicideItems.PUTRID_HIDE.get(), 0.1F, 200)
                 .unlockedBy("has_rotten_flesh", inventoryTrigger(ItemPredicate.Builder.item()
