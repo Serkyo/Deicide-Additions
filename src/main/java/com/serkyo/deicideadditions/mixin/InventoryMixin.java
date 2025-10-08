9adf543feb7ef2022b9fb694f20b03920e254ab9
Serkyo <prousttom@gmail.com>
2025-10-08 10:16:53 +0200

    Added a damage limit to armor, capped at 1% of their total durability

---
+++ src/main/java/com/serkyo/deicideadditions/mixin/InventoryMixin.java
@@ -0,0 +1,17 @@
+package com.serkyo.deicideadditions.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(Inventory.class)
+public class InventoryMixin {
+    @ModifyArg(method = "hurtArmor", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;hurtAndBreak(ILnet/minecraft/world/entity/LivingEntity;Ljava/util/function/Consumer;)V"))
+    public int armorDamageCap(int pAmount, @Local ItemStack itemStack) {
+        int maxDurabilityDamage = (int) Math.ceil(itemStack.getMaxDamage() * 0.01F);
+        return Math.min(pAmount, maxDurabilityDamage);
+    }
+}
