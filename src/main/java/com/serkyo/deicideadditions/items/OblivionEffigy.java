c9b6e823d940ed9213dc7e3471c27e1d6d93c45d
Serkyo <serkyo-dev@proton.me>
2025-10-09 09:45:47 +0200

    Added properties to OblivionEffigy

--- src/main/java/com/serkyo/deicideadditions/items/OblivionEffigy.java
+++ src/main/java/com/serkyo/deicideadditions/items/OblivionEffigy.java
@@ -1,6 +1,8 @@
 package com.serkyo.deicideadditions.items;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -15,32 +17,43 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.puffish.skillsmod.api.Category;
 import net.puffish.skillsmod.api.SkillsAPI;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Optional;
 
 public class OblivionEffigy extends Item {
     public OblivionEffigy(Properties pProperties) {
-        super(pProperties);
+        super(pProperties
+                .stacksTo(1)
+                .rarity(Rarity.EPIC));
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        ItemStack stack = pPlayer.getItemInHand(pUsedHand);
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        ItemStack stack = player.getItemInHand(usedHand);
 
-        pLevel.playSound(pPlayer, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.TOTEM_USE, SoundSource.PLAYERS, 1, 1);
-        if (!pLevel.isClientSide) {
-            Optional<Category> skillTree = SkillsAPI.getCategory(ResourceLocation.fromNamespaceAndPath("deicideadditions", "epic_fight"));
-            if (skillTree.isPresent()) {
+        level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.TOTEM_USE, SoundSource.PLAYERS, 1, 1);
+        if (!level.isClientSide) {
+            ResourceLocation skillTreeResourceLocation = ResourceLocation.fromNamespaceAndPath("deicideadditions", "epic_fight");
+            Optional<Category> skillTreeOptional = SkillsAPI.getCategory(skillTreeResourceLocation);
+
+            if (skillTreeOptional.isPresent()) {
+                Category skilltree = skillTreeOptional.get();
                 stack.shrink(1);
-                skillTree.get().resetSkills((ServerPlayer) pPlayer);
-                pPlayer.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 100, 0, false, true));
-                pPlayer.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 100, 0, false, true));
-                pPlayer.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 200, 0, false, true));
-                if (pLevel instanceof ServerLevel serverLevel) {
-                    serverLevel.sendParticles(ParticleTypes.TOTEM_OF_UNDYING, pPlayer.getX(), pPlayer.getY() + 1, pPlayer.getZ(), 50, 0.5, 1, 0.5, 0.1);
+                skilltree.resetSkills((ServerPlayer) player);
+                skilltree.setPoints((ServerPlayer) player, skillTreeResourceLocation, skilltree.getPoints((ServerPlayer) player, skillTreeResourceLocation) / 2);
+                player.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 100, 0, false, true));
+                player.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 100, 0, false, true));
+                player.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 200, 0, false, true));
+
+                if (level instanceof ServerLevel serverLevel) {
+                    serverLevel.sendParticles(ParticleTypes.TOTEM_OF_UNDYING, player.getX(), player.getY() + 1, player.getZ(), 50, 0.5, 1, 0.5, 0.1);
                 }
             }
         }
@@ -48,7 +61,7 @@ public class OblivionEffigy extends Item {
             triggerTotemAnimation(stack);
         }
 
-        return InteractionResultHolder.sidedSuccess(stack, pLevel.isClientSide);
+        return InteractionResultHolder.sidedSuccess(stack, level.isClientSide);
     }
 
     private void triggerTotemAnimation(ItemStack stack) {
@@ -56,6 +69,18 @@ public class OblivionEffigy extends Item {
 
         if (mc.level != null) {
             mc.gameRenderer.displayItemActivation(stack);
+        }
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag isAdvanced) {
+        tooltip.add(Component.translatable("item.deicideadditions.oblivion_effigy.description").withStyle(ChatFormatting.GRAY));
+        tooltip.add(Component.empty());
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Component.translatable( "item.deicideadditions.oblivion_effigy.lore").withStyle(ChatFormatting.BLUE, ChatFormatting.ITALIC));
+        }
+        else {
+            tooltip.add(Component.translatable("item.deicideadditions.showlore").withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC));
         }
     }
 }
