9f71d4024d0735d7fbea3f941bf377dc8262cbaf
Serkyo <prousttom@gmail.com>
2025-10-10 09:00:22 +0200

    Gambling !

--- src/main/java/com/serkyo/deicideadditions/effect/DespairEffect.java
+++ src/main/java/com/serkyo/deicideadditions/effect/DespairEffect.java
@@ -13,31 +13,31 @@ public class DespairEffect extends DeicideMobEffect {
         this.addAttributeModifier(
             Attributes.ARMOR,
             "f3e035d6-ae0e-467b-9014-750dc14ee584",
-            -0.9D,
+            -0.95D,
             AttributeModifier.Operation.MULTIPLY_TOTAL
         );
         this.addAttributeModifier(
             Attributes.ARMOR_TOUGHNESS,
             "e3f1ea42-525c-4770-8e96-1f27141647aa",
-            -0.9D,
+            -0.95D,
             AttributeModifier.Operation.MULTIPLY_TOTAL
         );
         this.addAttributeModifier(
             Attributes.ATTACK_DAMAGE,
             "5925ab3c-0597-4d1d-be28-69a25f46817a",
-            -0.9D,
+            -0.95D,
             AttributeModifier.Operation.MULTIPLY_TOTAL
         );
         this.addAttributeModifier(
             AttributeRegistry.SPELL_POWER.get(),
             "be0db7a1-2ff6-4011-8916-bcf5fd1a447d",
-            -0.9D,
+            -0.95D,
             AttributeModifier.Operation.MULTIPLY_TOTAL
         );
         this.addAttributeModifier(
             EntityAttributes_ProjectileDamage.GENERIC_PROJECTILE_DAMAGE,
             "8745f9b1-ecb3-46e7-8597-e460755540cc",
-            -0.9D,
+            -0.95D,
             AttributeModifier.Operation.MULTIPLY_TOTAL
         );
     }
