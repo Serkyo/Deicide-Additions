a7087f844cb6a5a13be28a7fc7a105bb342453e0
Serkyo <serkyo-dev@proton.me>
2025-11-26 22:34:11 +0100

    Changed effect values to make them more pushing and rewarding

--- src/main/java/com/serkyo/deicideadditions/mixin/MobEffectMixin.java
+++ src/main/java/com/serkyo/deicideadditions/mixin/MobEffectMixin.java
@@ -19,7 +19,7 @@ public class MobEffectMixin {
     @ModifyArg(method = "applyEffectTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;heal(F)V", ordinal = 0))
     public float modifyRegeneration(float defaultValue, @Local(argsOnly = true) LivingEntity entity) {
         if (entity instanceof Player player) {
-            float newAmount = (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth() / 3));
+            float newAmount = (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth() / 2));
             if (defaultValue != newAmount) {
                 DeicideAdditions.LOGGER.debug("Increased the healing received by {} from Regeneration to {}", player.getName().getString(), newAmount);
             }
@@ -43,7 +43,7 @@ public class MobEffectMixin {
     @ModifyArg(method = "applyEffectTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", ordinal = 1))
     public float modifyWither(float defaultValue, @Local(argsOnly = true) LivingEntity entity) {
         if (entity instanceof Player player){
-            float newAmount = (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth() / 3));
+            float newAmount = (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth() / 2));
             if (defaultValue != newAmount) {
                 DeicideAdditions.LOGGER.debug("Increased the damage received by {} from Wither to {}", player.getName().getString(), newAmount);
             }
@@ -98,7 +98,7 @@ public class MobEffectMixin {
     @ModifyArg(method = "applyInstantenousEffect", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", ordinal = 0))
     public float modifyInstantDamage2(float defaultValue, @Local(argsOnly = true) LivingEntity entity) {
         if (entity instanceof Player player) {
-            float newAmount = (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth() / 2));
+            float newAmount = (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth()));
             if (defaultValue != newAmount) {
                 DeicideAdditions.LOGGER.debug("Increased the damage received by {} from Instant Damage to {}", player.getName().getString(), newAmount);
             }
@@ -110,7 +110,7 @@ public class MobEffectMixin {
     @ModifyArg(method = "applyInstantenousEffect", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", ordinal = 1))
     public float modifyInstantDamage3(float defaultValue, @Local(argsOnly = true) LivingEntity entity) {
         if (entity instanceof Player player) {
-            float newAmount = (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth() / 2));
+            float newAmount = (float) Math.max(defaultValue, Math.ceil(defaultValue / 20 * player.getMaxHealth()));
             if (defaultValue != newAmount) {
                 DeicideAdditions.LOGGER.debug("Increased the damage received by {} from Instant Damage to {}", player.getName().getString(), newAmount);
             }
